ad394fa6ec6a010d8bec6dd49c42db453df8201a
Ka Ying Sally Wu <kayingwu@gmail.com>
2018-07-07 23:17:26 -0400

    Initial fend_proj_3

--- js/app.js
+++ js/app.js
@@ -24,13 +24,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function Player(character) {
+  this.img = character || 'images/char-boy.png';
+  this.x = 203;
+  this.y = 440;
+}
 
+Player.prototype.update = function() {
+
+}
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.img), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+let player = new Player();
+let allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
